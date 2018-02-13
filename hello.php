f52beb888e8ae584fa365466a12ffb0f64cd9baf
Robert McDonell <rmcdonell@yahoo.com>
2018-02-12 18:53:08 -0600

    Email push

--- hello.php
+++ hello.php
@@ -1,1 +1,9 @@
-t4wyrr
+<?php
+$to = "rmcdonell@yahoo.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: admin@gobobbygo.com" . "\r\n" .
+"CC: rmcdonell@dmacc.edu";
+
+mail($to,$subject,$txt,$headers);
+?>
