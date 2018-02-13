86221d283e25817d9df54b2e5c4c2111b8f169cc
Robert McDonell <rmcdonell@yahoo.com>
2018-02-13 17:22:20 -0600

    add sentFrom

--- emailerSent.php
+++ emailerSent.php
@@ -5,7 +5,8 @@ $newEmail = new Emailer(); //instantiation a new object/variable
 
 $newEmail->setSendTo("rmcdonell@dmacc.edu");
 
-echo $newEmail->getSentTo();
+echo $newEmail->getSendTo();
 
-
-?>
+$newEmail = new Emailer();
+$newEmail->setSentFrom("rmcdonell@yahoo.com");
+echo $newEmail->getSentFrom();
